f666257a6a7d4666c05b6f7f92e510ccb34a065d
afrajahmd2005-ai <afrajahmd2005@gmail.com>
2026-09-05 12:24:01 +0530

    add

--- types_arguments.py
+++ types_arguments.py
@@ -4,10 +4,23 @@
 
 # greet(name ="afrrj" , dept ="com")
 
-def add(*numbers): #(5,6,7)
-    c = 0
-    for i in numbers:
-        c = c+i
-    print(f"sum is {c}")
+# def add(*numbers): #(5,6,7)
+#     c = 0
+#     for i in numbers:
+#         c = c+i
+#     print(f"sum is {c}")
 
-add(5,6,7)
+# add(5,6,7)
+
+# def print_user_details(**details):
+#     for key , value in details.items():
+#         print(f"{key} : {value}")
+
+# print_user_details(name="Afraj", dept="CS", year=3)
+
+def display_info(title, *args, **kwargs):
+    print("Title:", title)
+    print("Args:", args)
+    print("Kwargs:", kwargs)
+
+display_info("Student Profile", "Tamil", "English", age=20, city="Chennai")
